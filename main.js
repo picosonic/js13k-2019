8d091f30ebf138c9b7d9c219763a3182fb8178dc
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2019-08-23 23:27:53 +0100

    Added RFC4122 UUIDs and simplified adding to scene

--- main.js
+++ main.js
@@ -107,9 +107,37 @@ function gamepadscan()
   window.requestAnimationFrame(gamepadscan);
 }
 
+// Deep clone an object
 function clone(obj)
 {
   return JSON.parse(JSON.stringify(obj));
+}
+
+// Generate a UUID v4 as per RFC 4122
+function uuidv4()
+{
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+    return v.toString(16);
+  });
+}
+
+// Add models to active models
+function addmodel(model, x, y, z, rotx, roty, rotz)
+{
+  var obj=clone(model);
+
+  obj.id=uuidv4();
+
+  obj.x=x;
+  obj.y=y;
+  obj.z=z;
+
+  obj.rotx=rotx;
+  obj.roty=roty;
+  obj.rotz=rotz;
+
+  gs.activemodels.push(obj);
 }
 
 // Entry point
@@ -125,15 +153,8 @@ function init()
   // Temporary clock
 //  setInterval(function(){ updatetime(); }, 1000);
 
-  gs.activemodels[0]=clone(models[0]);
-  gs.activemodels[0].x=0;
-  gs.activemodels[0].y=0;
-  gs.activemodels[0].z=0;
-
-  gs.activemodels[1]=clone(models[0]);
-  gs.activemodels[1].x=200;
-  gs.activemodels[1].y=200;
-  gs.activemodels[1].z=200;
+  addmodel(models[0], 0, 0, 0, 0, 0, 0);
+  addmodel(models[0], 200, 200, 200, 10, 10, 10);
 
   // Gamepad support
   if (!!(navigator.getGamepads))
