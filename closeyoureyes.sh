13dcaa45cc080d3a4ec771b9cbfefe7c738ccfce
Jasper Renow-Clarke <jrenow_clarke@yahoo.co.uk>
2021-08-26 17:06:00 +0100

    Updated to new closure download method

--- closeyoureyes.sh
+++ closeyoureyes.sh
@@ -6,16 +6,20 @@ dlname="compiler-latest.zip"
 # Check if we need closure locally
 if [ ! -d "${cpath}" ]
 then
-  mkdir "${cpath}" >/dev/null 2>&1
-  cd "${cpath}"
-  wget "https://dl.google.com/closure-compiler/${dlname}"
-  unzip "${dlname}"
-  rm "${dlname}"
-  cd ..
+  echo "Follow instructions at https://developers.google.com/closure/compiler/docs/gettingstarted_app"
+  echo "Downloading the latest JAR from https://mvnrepository.com/artifact/com.google.javascript/closure-compiler"
+  exit
+fi
+
+# Check for input script name
+if [ $# -ne 1 ]
+then
+  echo "Specify script on command line"
+  exit
 fi
 
 # Find out the latest jar filename
-compiler=`ls -rt closure/closure-compiler-*.jar`
+compiler=`ls -rt ${cpath}/closure-compiler-*.jar`
 
 # Use first parameter as JS file, write to stdout with ADVANCED compilation level
 java -jar "${compiler}" --compilation_level ADVANCED --js "$1"
